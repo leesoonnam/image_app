40e35e95c4210bfabd8b0b2802311ff08b768341
leesoonnam <132547276+leesoonnam@users.noreply.github.com>
2023-08-16 17:38:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 from rembg import remove
 from PIL import Image
+from streamlit_image_comparison import image_comparison
+import easyocr as ocr
+import numpy as np
 
+#set page config
+st.set_page_config(page+title = "My First_Image_App', layout ='centered')
 st.subheader('이미지 배경제거 서비스')
 
 uploaded_file = st.file_uploader("이미지를 업로드하세요", type=['png', 'jpg', 'jpeg'])
