ce79dacf36f5df36294112463333b355f9608d25
leesoonnam <132547276+leesoonnam@users.noreply.github.com>
2023-08-16 17:56:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ import numpy as np
 #set page config
 st.set_page_config(page_title = "My_First_Image_App", layout ='centered')
 st.subheader('[미니프로젝트]이미지 배경제거 + 글자추출 웹서비스')
-st.markdown('### :arrow_forward::sunglasses: Remove Background - 'rembg'')
+st.markdown("### :arrow_forward::sunglasses: Remove Background")
 st.markdown('#### sample result')
+
+
 
 uploaded_file = st.file_uploader("이미지를 업로드하세요", type=['png', 'jpg', 'jpeg'])
 
